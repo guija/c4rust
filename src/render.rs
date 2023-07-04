d4f00da51a9cdab2fbbfeb8f2b97fef35466c634
Guillermo Janner <guillermo.janner@vyoma.space>
2023-07-04 14:54:52 +0200

    Child and parent helper methods

--- src/render.rs
+++ src/render.rs
@@ -1,31 +1,47 @@
-use petgraph::{dot::Dot, stable_graph::NodeIndex};
+use petgraph::{dot::Dot, graph, stable_graph::NodeIndex, Graph};
 
-use crate::core::{self, EdgeType, Project};
+use crate::core::{self, Edge, EdgeType, Element, Project};
 
 pub fn default_dot(project: &core::Project) -> String {
     format!("{:?}", Dot::with_config(&project.graph, &[]))
 }
 
-pub fn dot(project: &Project, element_index: NodeIndex) {
-    let element = project.graph.node_weight(element_index);
+type C4Graph = Graph<Element, Edge>;
+type C4Index = NodeIndex;
 
-    let children: Vec<NodeIndex> = project
-        .graph
-        .neighbors(element_index)
-        .map(|neighbour| {
-            println!("neighbour {:?}", neighbour);
-            neighbour
-        })
-        .filter(|neighbour| {
-            project
-                .graph
-                .edges_connecting(element_index, *neighbour)
-                .any(|x| x.weight().edge_type == EdgeType::IsParentOf)
-        })
-        .collect();
-
+pub fn dot(project: &Project, element_index: C4Index) {
+    let graph = &project.graph;
+    let element = graph.node_weight(element_index);
+    let children = children(graph, element_index);
     for child_index in children {
         let child = project.graph.node_weight(child_index);
-        println!("{:?} is child of {:?}", child, element)
+        println!("{:?} is child of {:?}", child, element);
+        let parent = parent(graph, child_index);
+        println!("{:?} is parent of {:?}", parent, child);
     }
+}
+
+fn children(graph: &C4Graph, element_index: C4Index) -> Vec<C4Index> {
+    neighbours_with_directed_edge_type(graph, element_index, EdgeType::IsChildOf)
+}
+
+fn parent(graph: &C4Graph, element_index: C4Index) -> Option<C4Index> {
+    neighbours_with_directed_edge_type(graph, element_index, EdgeType::IsParentOf)
+        .first()
+        .cloned()
+}
+
+fn neighbours_with_directed_edge_type(
+    graph: &C4Graph,
+    from: C4Index,
+    edge_type: EdgeType,
+) -> Vec<C4Index> {
+    graph
+        .neighbors(from)
+        .filter(|neighbour| {
+            graph
+                .edges_connecting(*neighbour, from)
+                .any(|x| x.weight().edge_type == edge_type)
+        })
+        .collect()
 }
